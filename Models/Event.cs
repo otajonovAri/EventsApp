d6a1e2a9acf371e7e42e467cf1a132cdb6a48fc5
OtajonovAri <otajonovmuhammadali668@gmail.com>
2025-04-29 10:04:46 +0500

    full

---
+++ Models/Event.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventEaseApp.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+    }
+}
